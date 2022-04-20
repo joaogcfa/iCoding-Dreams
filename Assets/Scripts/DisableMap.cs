672594f4fc990884e34f1d203e88a82f9f598439
Joao Guilherme Almeida <joaogcfa@al.insper.edu.br>
2022-04-20 09:40:57 -0300

    next level loader e tentando mudar fracao do plano

---
+++ Assets/Scripts/DisableMap.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableMap : MonoBehaviour
+{
+    public bool isPressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isPressed = gameObject.GetComponent<PhysicsButton>().isPressed;
+        if (isPressed) Load();
+
+    }
+
+    public void Load()
+    {
+        GameObject.Find("Mapa").SetActive(false);
+        isPressed = false;
+
+    }
+}
---
+++ Assets/Scripts/NextLevelLoader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // print(collision.gameObject.name);
+        // if (collision.gameObject.name == "Player")
+        // {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // }
+    }
+}
