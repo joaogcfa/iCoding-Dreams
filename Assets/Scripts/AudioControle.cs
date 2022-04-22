b75db77415b38721d4d237c7d40d6f0c33905f25
DiogoPLobo <92478902+DiogoPLobo@users.noreply.github.com>
2022-04-22 15:08:26 -0300

    alterando a velocidade do tick

--- Assets/Scripts/AudioControle.cs
+++ Assets/Scripts/AudioControle.cs
@@ -6,7 +6,8 @@ public class AudioControle : MonoBehaviour
 {
     public AudioSource audioSourceMusicaClock;
     public AudioClip[] MusicasDeFundo; //caso queiram colocar mais de uma música de fundo
-
+    public float velocidade = 0;
+    public int qualfreq = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +18,21 @@ public class AudioControle : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        int i = 0.01
+        velocidade(i++);
+        if (velocidade == 1000)
+        {
+            velocidade = 0;
+            qualfreq++;
+            AudioClip MusicaDessaFase = MusicasDeFundo[qualfreq];
+            audioSourceMusicaClock.clip = MusicaDessaFase;
+            audioSourceMusicaClock.Play();
+        }
+
+        if(qualfreq == 16)
+        {
+            qualfreq = 0;
+        }
     }
 }
