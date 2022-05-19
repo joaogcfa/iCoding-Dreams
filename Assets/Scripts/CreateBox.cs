c5bd6b00482bb6bf3562977cf19e31d041c3fd21
Joao Guilherme Almeida <joaogcfa@al.insper.edu.br>
2022-05-19 15:33:29 -0300

    contador com ui

--- Assets/Scripts/CreateBox.cs
+++ Assets/Scripts/CreateBox.cs
@@ -1,23 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CreateBox : MonoBehaviour
 {
 
     public GameObject Box;
     private Vector3 pos;
+    public int max = 1;
+    public int count;
+    TMP_Text FruitCounter;
+    // private GameObject CounterText;
 
     // Start is called before the first frame update
     void Start()
     {
-        print(gameObject.transform);
+        count = 0;
+        // print(gameObject.transform.Find("Counter"));
+        FruitCounter = GameObject.Find("Counter").GetComponent<TMP_Text>();
+        print(FruitCounter);
+        FruitCounter.SetText("0/{0}", max);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(count >= max){
+            Destroy(gameObject.transform.parent.gameObject);
+        }
         
     }
 
@@ -29,6 +40,8 @@ public class CreateBox : MonoBehaviour
             pos = GameObject.Find("Player").transform.position;
             pos += new Vector3 (0,2,4);
             Instantiate(Box, pos, Quaternion.identity);
+            count++;
+            FruitCounter.SetText("{0}/{1}", count, max);
             
         }
     }
